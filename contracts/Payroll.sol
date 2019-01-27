535d666136dbd415b3c7c437c81334ee966246f6
Clyde Vassallo <clyde.vassallo@gig.com>
2019-01-27 12:40:00 +0100

    Track employee punched state and add checks for channel state

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -17,6 +17,8 @@ contract Payroll is Ownable {
 
     mapping (address => OneTimeChannel) channels;
 
+    mapping (uint => bool) isEmployeePunchedIn;
+
     /* Function Modifiers */
 
     modifier onlyEmployee() {
@@ -40,8 +42,16 @@ contract Payroll is Ownable {
 
     function releaseChannel(uint _employeeId) 
     public onlyOwner {
+        require (address(channels[employee]) != address(0));
+        
+        // Set employee as punched out, should be redundant but ensure that the state is not corrupted
+        if (isEmployeePunchedIn[_employeeId]) {
+            isEmployeePunchedIn[_employeeId] = false;
+        }
+
         address employee = employeeContractStorage.readEmployeeContractIncomeAccount(_employeeId);
         channels[employee].timeOutChannel();
+        channels[employee] = OneTimeChannel(0);
     }
 
     // Callable By Employee //
@@ -50,6 +60,13 @@ contract Payroll is Ownable {
     public onlyEmployee {
         // Get employee id
         uint employeeId = employeeContractStorage.readEmployeeId(msg.sender);
+
+        // Ensure employee is not punched in and punch in
+        require(isEmployeePunchedIn[employeeId] == false);
+        isEmployeePunchedIn[employeeId] = true;
+
+        // Ensure a channel does not already exist for this employee
+        require(channels[msg.sender] == OneTimeChannel(0), "A channel already exists. Probably a punch out was missed. Ask employer to timeout the channel.");
 
         // Get Salary Per Hour 
         uint employeeSalaryPerHour = employeeContractStorage.readHourlySalary(employeeId);
@@ -69,13 +86,22 @@ contract Payroll is Ownable {
         // Calculate maximum salary for day
         uint employeeMaximumSalaryPerDay = employeeSalaryPerHour * employeeMaximumHoursPerDay;
 
+        // Open the payment channel
         channels[msg.sender] = OneTimeChannel((new OneTimeChannel).value(employeeMaximumSalaryPerDay)(address(this), msg.sender, timeOutValue));    
     }
 
     function punchOut(bytes32 _hash, bytes _signature) 
     public onlyEmployee {
+        // Get employee id
+        uint employeeId = employeeContractStorage.readEmployeeId(msg.sender);
+
+        // Ensure employee is punched in and punch out
+        require(isEmployeePunchedIn[employeeId] == true);
+        isEmployeePunchedIn[employeeId] = false;
+
         uint value = 10; // should this be the calculation based on rate?
         channels[msg.sender].closeChannel(_hash, _signature, value);
+        channels[msg.sender] = OneTimeChannel(0);
     }
 
     // default fn
