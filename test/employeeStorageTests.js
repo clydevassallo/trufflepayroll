0009d0eec80f124cf344aac2241fd6f1bcd29c94
Clyde Vassallo <clyde.vassallo@gig.com>
2019-02-21 20:41:34 +0100

    Add cleaning state function

--- test/employeeStorageTests.js
+++ test/employeeStorageTests.js
@@ -1,7 +1,41 @@
 var EmployeeContractStorage = artifacts.require('./EmployeeContractStorage.sol');
 
+// https://truffleframework.com/docs/truffle/testing/testing-your-contracts#clean-room-environment
+// ^ Should be at a clean state
 contract('EmployeeContractStorage', function (accounts) {
     
+    it('should not be accessible by accounts outside the whitelist', function() {
+        let employeeContractStorage;
+        return EmployeeContractStorage
+            .deployed()
+            .then(function(instance) {
+                employeeContractStorage = instance;
+                return employeeContractStorage.addAddressToWhitelist(accounts[0]);
+            }).then(function () {
+                return employeeContractStorage.createEmployeeContract(accounts[1], 100, 8, {from: accounts[3]});
+            }).then(function () {
+                assert.equal(true,false,"createEmployeeContract function should not be accessible outside the whitelist"); 
+            }).catch(function () {
+                assert.equal(true,true);
+            });
+    });
+
+    it('should be accessible by accounts inside the whitelist', function() {
+        let employeeContractStorage;
+        return EmployeeContractStorage
+            .deployed()
+            .then(function(instance) {
+                employeeContractStorage = instance;
+                return employeeContractStorage.addAddressToWhitelist(accounts[0]);
+            }).then(function () {
+                return employeeContractStorage.createEmployeeContract(accounts[1], 100, 8, {from: accounts[0]});
+            }).then(function () {
+                assert.equal(true,true);
+            }).catch(function () {
+                assert.equal(true,false,"createEmployeeContract function should be accessible inside the whitelist"); 
+            });
+    });
+
     it('should create the employee as specified', function() {
         let employeeContractStorage;
         let employeeCount;
@@ -18,7 +52,7 @@ contract('EmployeeContractStorage', function (accounts) {
                 employeeCount = _employeeCount;
 
                 // Given I create an employee contract for an account
-                return employeeContractStorage.createEmployeeContract(accounts[1], 100, 8);
+                return employeeContractStorage.createEmployeeContract(accounts[2], 100, 8);
             }).then(function () {
                 // When I retrieve the employee count
                 return employeeContractStorage.getNumberOfEmployees.call();    
@@ -28,16 +62,52 @@ contract('EmployeeContractStorage', function (accounts) {
                 assert.equal(Number(employeeCount) + 1, _employeeCount);
               
                 // When I retrieve the hourly salary
-                return employeeContractStorage.readSalaryPerSecond.call(accounts[1]);
+                return employeeContractStorage.readSalaryPerSecond.call(accounts[2]);
             }).then(function (salaryPerSecond) {
                 // Then the hourly salary is as expected
                 assert.equal(100, salaryPerSecond, "Salary per second was not as expected but was " + salaryPerSecond);
 
                 // When I retrieve the maximum hours per day
-                return employeeContractStorage.readMaximumSecondsPerSession.call(accounts[1]);
+                return employeeContractStorage.readMaximumSecondsPerSession.call(accounts[2]);
             }).then(function (maximumSecondsPerSession) {
                 // Then the hourly salary is as expected
                 assert.equal(8, maximumSecondsPerSession, "Maximum Hours Per Day was not as expected but was " + maximumSecondsPerSession);
+            });
+    });
+
+    it('should not allow the creation of duplicate employees', function() {
+        let employeeContractStorage;
+        return EmployeeContractStorage
+            .deployed()
+            .then(function(instance) {
+                employeeContractStorage = instance;
+                return employeeContractStorage.addAddressToWhitelist(accounts[0]);
+            }).then(function () {
+                return employeeContractStorage.createEmployeeContract(accounts[3], 100, 8, {from: accounts[0]});
+            }).then(function () {
+                return employeeContractStorage.createEmployeeContract(accounts[3], 100, 8, {from: accounts[0]});
+            }).then(function () {
+                assert.equal(true,false,"Duplicate employee creation should have failed."); 
+            }).catch(function () {
+                assert.equal(true,true);
+            });
+    });
+
+    it('should update the salary correctly', function() {
+        let employeeContractStorage;
+        return EmployeeContractStorage
+            .deployed()
+            .then(function(instance) {
+                employeeContractStorage = instance;
+                return employeeContractStorage.addAddressToWhitelist(accounts[0]);
+            }).then(function () {
+                return employeeContractStorage.createEmployeeContract(accounts[4], 100, 8, {from: accounts[0]});
+            }).then(function () {
+                return employeeContractStorage.updateSalaryPerSecond(accounts[4], 200, {from: accounts[0]});
+            }).then(function() {
+                return employeeContractStorage.readSalaryPerSecond.call(accounts[4]);
+            }).then(function (salaryPerSecond) {
+                assert.equal(200, salaryPerSecond, "Salary per second was not as expected but was " + salaryPerSecond);
             });
     });
     
--- test/payrollTests.js
+++ test/payrollTests.js
@@ -1,85 +1,116 @@
 var Payroll = artifacts.require('./Payroll.sol');
+var EmployeeContractStorage = artifacts.require('./EmployeeContractStorage.sol');
 
+// https://truffleframework.com/docs/truffle/testing/testing-your-contracts#clean-room-environment
+// ^ Should be at a clean state
 contract('Payroll', function (accounts) {
-    let payroll;
-    it('should create the employee and the employee should be employed', function() {
-        return Payroll
-        .deployed()
-        .then(function (instance) {
-            payroll = instance;
-            return payroll.hireEmployee(accounts[2], 10, 8);
-        }).then(function() {
-            return payroll.isEmployed.call(accounts[2]);
-        }).then(function(isEmployed) {
-            assert.equal(isEmployed, true);
+
+    describe("Clean Payroll Tests", function() {
+        // Clean the employee contract storage as it was not being cleaned even though it should have been.
+        before("Initial State", function () {
+            let payroll
+            let employeeContractStorage;
+            return Payroll
+            .deployed()
+            .then(function (instance) {
+                payroll = instance;
+                return EmployeeContractStorage.new({from: accounts[0]});
+            }).then(function (instance) {
+                employeeContractStorage = instance;
+                return payroll.setEmployeeContractStorage(instance.address, {from: accounts[0]});
+            }).then(function() {
+                employeeContractStorage.addAddressToWhitelist(payroll.address);
+            });
         });
-    });
 
-    it('should manage the balance', function() {
-        let initialBalance;
-        return Payroll
-        .deployed()
-        .then(function (instance) {
-            payroll = instance;
-            return payroll.getBalance.call();
-        }).then(function(balance) {
-            initialBalance = balance;
-
-            // Deposit
-            return payroll.addFunds({value: 100});
-        }).then(function() {
-            return payroll.getBalance.call();
-        }).then(function(balance) {
-            // Check balance
-            assert.equal(Number(initialBalance) + 100, balance);
-
-            // Withdraw
-            return payroll.withdrawFunds(50);
-        }).then(function() {
-            return payroll.getBalance.call();
-        }).then(function(balance){
-            // Check balance
-            assert.equal(Number(initialBalance) + 50, balance);
+        it('should create the employee and the employee should be employed', function() {
+            let payroll
+            return Payroll
+            .deployed()
+            .then(function (instance) {
+                payroll = instance;
+                return payroll.countEmployees.call();
+            }).then(function (employeeCount){
+                console.log('There are '+ employeeCount + ' employees!!!')
+            
+                return payroll.hireEmployee(accounts[1], 10, 8);
+            }).then(function() {
+                return payroll.isEmployed.call(accounts[1]);
+            }).then(function(isEmployed) {
+                assert.equal(isEmployed, true);
+            });
         });
-    });
 
-    it('should not allow employees to punch in with insufficient balance', function() {
-        return Payroll
-        .deployed()
-        .then(function (instance) {
-            payroll = instance;
-            return payroll.getBalance.call();
-        }).then(function(balance) {
-            return payroll.hireEmployee(accounts[3], balance, 2);
-        }).then(function() {
-            return payroll.punchIn({from: accounts[3]});
-        }).then(function() {
-            assert.equal(true,false,"PunchIn should have failed with insufficient balance but didn't");
-        }).catch(function(e) {
-            let errorMessage = e.message;
-            assert.equal(true, errorMessage.includes('not enough money'));
-        }).then(function() {
-            return payroll.isPunchedIn.call({from: accounts[3]});
-        }).then(function(isPunchedIn){
-            assert.isFalse(isPunchedIn, "Expected employee to not be punched in but was.")
-        });
-    });
+        it('should manage the balance', function() {
+            let initialBalance;
+            let payroll
+            return Payroll
+            .deployed()
+            .then(function (instance) {
+                payroll = instance;
+                return payroll.getBalance.call();
+            }).then(function(balance) {
+                initialBalance = balance;
 
-    it('should allow employees to punch in with sufficient balance', function() {
-        return Payroll
-        .deployed()
-        .then(function (instance) {
-            payroll = instance;
-            return payroll.getBalance.call();
-        }).then(function(balance) {
-            return payroll.hireEmployee(accounts[4], balance/4, 2);
-        }).then(function() {
-            return payroll.punchIn({from: accounts[4]});
-        }).then(function() {
-            return payroll.isPunchedIn.call({from: accounts[4]});
-        }).then(function(isPunchedIn) { 
-            assert.isOk(isPunchedIn, "Expected employee to be punched in but was not.");
+                // Deposit
+                return payroll.addFunds({value: 100});
+            }).then(function() {
+                return payroll.getBalance.call();
+            }).then(function(balance) {
+                // Check balance
+                assert.equal(Number(initialBalance) + 100, balance);
+
+                // Withdraw
+                return payroll.withdrawFunds(50);
+            }).then(function() {
+                return payroll.getBalance.call();
+            }).then(function(balance){
+                // Check balance
+                assert.equal(Number(initialBalance) + 50, balance);
+            });
         });
+
+        it('should not allow employees to punch in with insufficient balance', function() {
+            let payroll
+            return Payroll
+            .deployed()
+            .then(function (instance) {
+                payroll = instance;
+                return payroll.getBalance.call();
+            }).then(function(balance) {
+                return payroll.hireEmployee(accounts[2], balance, 2);
+            }).then(function() {
+                return payroll.punchIn({from: accounts[2]});
+            }).then(function() {
+                assert.equal(true,false,"PunchIn should have failed with insufficient balance but didn't");
+            }).catch(function(e) {
+                let errorMessage = e.message;
+                assert.equal(true, errorMessage.includes('not enough money'));
+            }).then(function() {
+                return payroll.isPunchedIn.call({from: accounts[2]});
+            }).then(function(isPunchedIn){
+                assert.isFalse(isPunchedIn, "Expected employee to not be punched in but was.")
+            });
+        });
+
+        it('should allow employees to punch in with sufficient balance', function() {
+            let payroll
+            return Payroll
+            .deployed()
+            .then(function (instance) {
+                payroll = instance;
+                return payroll.getBalance.call();
+            }).then(function(balance) {
+                return payroll.hireEmployee(accounts[3], balance/4, 2);
+            }).then(function() {
+                return payroll.punchIn({from: accounts[3]});
+            }).then(function() {
+                return payroll.isPunchedIn.call({from: accounts[3]});
+            }).then(function(isPunchedIn) { 
+                assert.isOk(isPunchedIn, "Expected employee to be punched in but was not.");
+            });
+        });
+
     });
 
 });
